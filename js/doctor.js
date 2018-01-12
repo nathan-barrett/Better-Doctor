578dd05fa3126daa9a3eedddc7452751b70c84db
nathan-barrett <demeules.barrett@gmail.com>
2018-01-12 10:28:57 -0800

    add info to console, able to push to front end

--- js/doctor.js
+++ js/doctor.js
@@ -1,5 +1,4 @@
 const apiKey = require('./../.env').apiKey;
-// import { pushResults } from './../doctor-interface.js';
 
 
 export class Doctor{
@@ -12,10 +11,23 @@ export class Doctor{
   }
 }
 export function apiCall(pushResults) {
-    console.log(apiKey);
+    const doctorInfo = [];
     const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=cold&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
-    $.get(url).then(function(data){
-      pushResults(data);
+    $.get(url).then(function(response){
+      console.log(response.data);
+      const doctorName = response.data[0].profile.first_name + " " + response.data[0].profile.middle_name + ". " + response.data[0].profile.last_name +  " " + response.data[0].profile.title;
+      const doctorAddress = response.data[0].practices[0].visit_address.street + " " + response.data[0].practices[0].visit_address.city + ", " + response.data[0].practices[0].visit_address.state + " " + response.data[0].practices[0].visit_address.zip;
+      const doctorPhone = response.data[0].practices[0].phones[0].number;
+      const doctorNew = response.data[0].practices[0].accepts_new_patients;
+      const doctorImg = "<img src=" + response.data[0].profile.image_url + ">";
+      function newPatientCheck() {
+        if (doctorNew === true) {
+          return "Currently accepting new patients.";
+        } else {
+          "Not currently accepting new patients.";
+        }
+      }
+      console.log(doctorName, doctorAddress, doctorPhone, newPatientCheck(), doctorImg);
     }).catch(function(error) {
       console.log(error);
     });
