07e13ec7042f7319c85f5c810834f165d2e6611e
nathan-barrett <demeules.barrett@gmail.com>
2018-01-12 11:45:20 -0800

    add results to index.html on click

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,10 +5,32 @@ $(document).ready(function(){
     event.preventDefault();
     const query = $("input#problem").val();
     let doctor = new Doctor("Nathan", query);
-    apiCall(pushResults);
+    apiCall(pushResults, query);
   });
-    function pushResults(data) {
-    console.log(data);
+    function pushResults(response, array) {
+    if (response.meta.count === 0) {
+      alert("it looks like your search didnt receive any results");
+    } else {
+      for(let i = 0; i < response.data.length; i++){
+        let doctorNew = response.data[i].practices[0].accepts_new_patients;
+        function newPatientCheck() {
+          if ( doctorNew === true) {
+            return "Currently accepting new patients.";
+          } else {
+            return "Not currently accepting new patients.";
+          }
+        }
+        $("#results").append(
+        `<div class="doctor col-md-4">
+          <h2 class="name">
+          ${response.data[i].profile.first_name}  ${response.data[i].profile.last_name}</h2>
+          <img class="image" src=${response.data[i].profile.image_url}>
+          <p class="new-patients">${newPatientCheck()}
+          <p class="street-address">${response.data[i].practices[0].visit_address.street}</p>
+          <p class="city-state">${response.data[i].practices[0].visit_address.city},  ${response.data[i].practices[0].visit_address.state} ${response.data[0].practices[0].visit_address.zip}</p>
+        </div>`
+        );
+      }
+    }
   }
-
 });
--- js/doctor.js
+++ js/doctor.js
@@ -5,29 +5,32 @@ export class Doctor{
   constructor(name, query) {
     this.name = name;
     this.query = query;
-    this.url = apiKey;
     // this.first = first;
     // this.last = last;
   }
 }
-export function apiCall(pushResults) {
+export function apiCall(pushResults, userQuery) {
     const doctorInfo = [];
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=cold&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
+    const doctorInfo2 = [];
+
+    const query = `query=${userQuery}`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?${query}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`;
     $.get(url).then(function(response){
-      console.log(response.data);
-      const doctorName = response.data[0].profile.first_name + " " + response.data[0].profile.middle_name + ". " + response.data[0].profile.last_name +  " " + response.data[0].profile.title;
+      if (response.data.length === 5);
+      const doctorName = response.data[0].profile.first_name + " " + response.data[0].profile.last_name +  " " + response.data[0].profile.title;
       const doctorAddress = response.data[0].practices[0].visit_address.street + " " + response.data[0].practices[0].visit_address.city + ", " + response.data[0].practices[0].visit_address.state + " " + response.data[0].practices[0].visit_address.zip;
       const doctorPhone = response.data[0].practices[0].phones[0].number;
       const doctorNew = response.data[0].practices[0].accepts_new_patients;
-      const doctorImg = "<img src=" + response.data[0].profile.image_url + ">";
+      const doctorImg = response.data[0].profile.image_url;
       function newPatientCheck() {
         if (doctorNew === true) {
           return "Currently accepting new patients.";
         } else {
-          "Not currently accepting new patients.";
+          return "Not currently accepting new patients.";
         }
       }
-      console.log(doctorName, doctorAddress, doctorPhone, newPatientCheck(), doctorImg);
+      doctorInfo.push(doctorName, doctorAddress, doctorPhone, newPatientCheck(), doctorImg);
+      pushResults(response, doctorInfo);
     }).catch(function(error) {
       console.log(error);
     });
